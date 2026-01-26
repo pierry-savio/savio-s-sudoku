b4d36284b735fc1bd43401151b01143b3597fcfe
pierry-savio <pierrysavio.pi@gmail.com>
2026-01-26 16:26:28 -0300

    docs: add readme

---
+++ readme.mk
@@ -0,0 +1,11 @@
+# Savio's Sudoku 📝
+
+A Sudoku website in HTML, CSS, and JavaScript where you can create your own game from scratch, made especially for my father, Savio.
+
+My father loves playing Sudoku. So, to practice and learn more about website creation, I decided to create this application for him. But instead of making a regular game, I suggested a challenge: creating his own game from scratch.
+
+Although it seems easy, it's not. A Sudoku game requires following a series of rigorous steps, and if you don't follow them, the game may become invalid.
+
+If you feel capable of creating your own game, you can try by clicking the link below 👇
+
+[Savio's Sudoku Website](https://pierry-savio.github.io/savio-s-sudoku/)
